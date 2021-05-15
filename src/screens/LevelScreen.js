5fca19d22d15a055c7179598e5a5f8ab6aa774a0
romservik29 <andrei.romservik29@gmail.com>
2021-05-15 19:27:40 +0300

    LevelScreen Pressable

--- src/screens/LevelScreen.js
+++ src/screens/LevelScreen.js
@@ -1,30 +1,32 @@
 import React from "react";
-import { Image, View, LinearGradient } from "react-native";
+import { Image, View, Pressable } from "react-native";
 import styled from "styled-components";
 import { Container } from "../components/util/Container";
 import { LinearProgress } from "react-native-elements";
 import StoryContainer from "../components/StoryContainer";
 
-export default function LevelScreen({ navigatoin, route }) {
+export default function LevelScreen({ navigation, route }) {
   return (
     <Container>
       <StoryContainer>
-        <View style={{ display: "flex", alignItems: "center" }}>
-          <View
-            style={{ display: "flex", alignItems: "center", paddingTop: 50 }}>
-            <Image
-              source={require("../img/Ellipse.png")}
-              style={{ width: 250, height: 250 }}
+        <Pressable onPress={() => navigation.navigate("HomeScreen")}>
+          <View style={{ display: "flex", alignItems: "center" }}>
+            <View
+              style={{ display: "flex", alignItems: "center", paddingTop: 50 }}>
+              <Image
+                source={require("../img/Ellipse.png")}
+                style={{ width: 250, height: 250 }}
+              />
+              <Level>LEVEL {route.params.level}</Level>
+            </View>
+            <LinearProgress
+              color='red'
+              variant='determinate'
+              value={route.params.progress}
+              style={{ height: 10, width: "80%" }}
             />
-            <Level>LEVEL {route.params.level}</Level>
           </View>
-          <LinearProgress
-            color='red'
-            variant='determinate'
-            value={route.params.progress}
-            style={{ height: 10, width: "80%" }}
-          />
-        </View>
+        </Pressable>
       </StoryContainer>
     </Container>
   );
